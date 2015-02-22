0cff511e0dd8d2113f1969a5e2ea7259ca495309
Nicholas Miles <namiles@Nicholass-MacBook-Pro.local>
2015-02-22 14:01:22 -0500

    Updated.  Added map data structure

--- galaxy.h
+++ galaxy.h
@@ -1,12 +1,15 @@
 #ifndef GALAXY
 #define GALAXY
 
+#include <vector>
+
 class Galaxy {
 
+    std::vector<vector<SolarSystem>> galaxy_map;
     int x_parsecs, y_parsecs;
-    void setupNewGame (void);
 
-    
+    void setupNewGame (void);
+ 
   public:
     Galaxy (int x_parsecs = 30, int y_parsecs = 30); 
 };
