5d7615ba5dbaac96dab40b816568f616ebd5280c
Nicholas Miles <namiles@Nicholass-MacBook-Pro.local>
2015-02-24 20:35:28 -0500

    Updated

--- solar_system.h
+++ solar_system.h
@@ -2,24 +2,26 @@
 #define SOLAR_SYSTEM
 
 #include <iostream>
-/*
 #include "celestial_object.h"
+#include "sun.h"
+/*
 #include "nebula.h"
 #include "drone.h"
 #include "ship.h"
 #include "planet.h"
 #include "asteroid.h"
 #include "wormhole.h"
-#include "sun.h"
 */
 
 class SolarSystem {
 
   static const int num_types = 3;
 
+  int x_pos, y_pos;
+
   public:
     int type;
-    SolarSystem (void); 
+    SolarSystem (int x, int y); 
 
 };
 
