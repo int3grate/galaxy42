dd775525fc9b5f1c02bea37f9f5e6012fa749dcb
Nicholas Miles <namiles@Nicholass-MacBook-Pro.local>
2015-03-01 15:23:10 -0500

    Started work on ships

---
+++ ship.cpp
@@ -0,0 +1,6 @@
+#include "ship.h"
+
+Ship::Ship(int x, int y, SolarSystem *s) : CelestialObject(x, y, s)
+{
+
+}
--- ship.h
+++ ship.h
@@ -1,8 +1,20 @@
 #ifndef SHIP
 #define SHIP
 
-class Ship : public CelestialObject {
+#include "galaxy.h"
 
+class Ship : public CelestialObject {
+    int x_pos;
+    int y_pos;
+
+  public:
+    bool canMoveWarp(int new_x_pos, int new_y_pos);
+    bool canMoveImpulse(int new_x_pos, int new_y_pos);
+
+    bool moveWarp(int new_x_pos, int new_y_pos);
+    bool moveImpulse(int new_x_pos, int new_y_pos);
+ 
+    Ship(int x, int y, SolarSystem *s); 
 };
 
 #endif
